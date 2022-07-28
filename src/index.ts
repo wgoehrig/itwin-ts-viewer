fc5f5814a875de6d375247bab2ba8218aa6cd9f8
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2022-07-28 12:03:56 -0400

    Add some comments

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import { FrontendIModelsAccess } from "@itwin/imodels-access-frontend";
 import { IModelsClient } from "@itwin/imodels-client-management";
 declare const ENV: any; // injected from .env
 
+/** Setup auth and login */
 const redirectUri = window.location.href;
 await BrowserAuthorizationCallbackHandler.handleSigninCallback(redirectUri);
 const authClient = new BrowserAuthorizationClient({ clientId: ENV.CLIENT_ID, scope: ENV.SCOPES, redirectUri });
 await authClient.signIn()
 
+/** Application initialization */
 await IModelApp.startup({
   authorizationClient: authClient,
   hubAccess: new FrontendIModelsAccess(new IModelsClient()),
@@ -18,16 +20,16 @@ await IModelApp.startup({
   },
 });
 
+/** Setup iModel Access Service client */
 const rpcConfig = { uriPrefix: "https://api.bentley.com", info: { title: "imodel/rpc", version: "" } };
 BentleyCloudRpcManager.initializeClient(rpcConfig, [IModelReadRpcInterface, IModelTileRpcInterface]);
 
+/** Connect to iModel Access Service */
 const iModelConnection = await CheckpointConnection.openRemote(ENV.ITWIN_ID, ENV.IMODEL_ID);
 
+/** Setup and add Viewport */
 const viewCreator = new ViewCreator3d(iModelConnection);
 const viewState = await viewCreator.createDefaultView({ skyboxOn: true });
 const root = document.getElementById("root") as HTMLDivElement;
 const vp = ScreenViewport.create(root, viewState);
 IModelApp.viewManager.addViewport(vp);
-
-IModelApp.tools.run(StandardViewTool.toolId, vp, StandardViewId.RightIso);
-IModelApp.tools.run(FitViewTool.toolId, vp, true, false);
